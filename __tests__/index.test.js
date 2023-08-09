8a6d644457d411db9a1fa2f1fd055839dc6ba551
Natalia Okhrimenko <ohrim2228n@mail.ru>
2023-08-09 17:32:37 +0300

    added tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,15 @@
 import {
-  analyzeText,
-  findLongestWord,
-  countSentences,
-  countUniqueWords,
-  displaySortedWords,
+  getTextAnalysis,
   clearTable,
-  clearText
+  clearText,
+  getFirstCharacter,
+  getCountWords,
+  getNumOfChar,
+  getMaxLengthWord,
+  getReverseWord,
+  getCountSentences,
+  getCountUniqueWord,
+  getAlphabeticalOrder,
 } from "../src/js/index";
 
 // Создаем фиктивный HTML для тестирования
@@ -72,14 +76,16 @@ document.body.innerHTML = `
 
 // Функция для добавления слушателей событий
 function addEventListeners() {
-  const analyzeButton = document.getElementById("analyzeButton");
-  analyzeButton.addEventListener("click", function (event) {
+  // проверить querySelector
+  const btnAanalysis = document.getElementById("analyzeButton");
+  const btnClear = document.getElementById("clearButton");
+
+  btnAanalysis.addEventListener("click", function (event) {
     event.preventDefault();
-    analyzeText();
+    getTextAnalysis();
   });
 
-  const clearButton = document.getElementById("clearButton");
-  clearButton.addEventListener("click", function (event) {
+  btnClear.addEventListener("click", function (event) {
     event.preventDefault();
     clearText();
     clearTable();
@@ -94,40 +100,46 @@ beforeEach(() => {
 describe("Анализ текста", () => {
   // Тест на анализ текста
   test("Анализировать текст", () => {
-    const analyzeTitle = document.getElementById("analyze-title");
-    const answer1 = document.getElementById("answer1");
-    const answer2 = document.getElementById("answer2");
-    const answer3 = document.getElementById("answer3");
-    const answer4 = document.getElementById("answer4");
-    const answer5 = document.getElementById("answer5");
-    const answer6 = document.getElementById("answer6");
-    const answer7 = document.getElementById("answer7");
-    const answer8 = document.getElementById("answer8");
-    const userText = document.getElementById("userText");
+    const userText = document.querySelector("#analyze-title");
+    const setUserText = document.querySelector("#userText");
+    const firstCharacter = document.querySelector("#answer5");
+    const countWords = document.querySelector("#answer1");
+    const numOfChar = document.querySelector("#answer2");
+    const longestWord = document.querySelector("#answer3");
+    const reverseWord = document.querySelector("#answer4");
+    const countSentences = document.querySelector("#answer6");
+    const countUniqueWord = document.querySelector("#answer7");
+    const alphabeticalOrder = document.querySelector("#answer8");
 
     // Вводим текст для анализа
-    analyzeTitle.value = "This is a test sentence. Another test sentence!";
+    userText.value = "This is a test sentence. Another test sentence!";
 
     // Вызываем функцию анализа
-    analyzeText();
+    getTextAnalysis();
 
     // Проверяем результаты анализа
-    expect(answer1.textContent).toBe("10");
-    expect(answer2.textContent).toBe("49");
-    expect(answer3.textContent).toBe("sentence");
-    expect(answer4.textContent).toBe("!ecnetnes tset rehtona tsuJ");
-    expect(answer5.textContent).toBe("T");
-    expect(answer6.textContent).toBe("2");
-    expect(answer7.textContent).toBe("10");
-    expect(answer8.textContent).toBe(
+    expect(firstCharacter.textContent).toBe("T");
+
+    //проверить
+    expect(countWords.textContent).toBe("10");
+    //проверить
+    expect(numOfChar.textContent).toBe("49");
+
+    expect(longestWord.textContent).toBe("sentence");
+    expect(reverseWord.textContent).toBe(
+      "!ecnetnes tset rehtonA .ecnetnes tset a si sihT"
+    );
+    expect(countSentences.textContent).toBe("2");
+    expect(countUniqueWord.textContent).toBe("7");
+    expect(alphabeticalOrder.textContent).toBe(
       "Another, This, a, is, sentence, test, tsuJ"
     );
 
     // Проверяем очистку текстового поля
-    expect(analyzeTitle.value).toBe("");
+    expect(userText.value).toBe("");
 
     // Проверяем текст, который был помещен в <p> элемент
-    expect(userText.textContent).toBe(
+    expect(setUserText.textContent).toBe(
       "This is a test sentence. Another test sentence!"
     );
   });
@@ -136,69 +148,69 @@ describe("Анализ текста", () => {
 // Тест на поиск самого длинного слова
 test("Поиск самого длинного слова", () => {
   const words = ["This", "is", "a", "test", "sentence"];
-  const longestWord = findLongestWord(words);
+  const longestWord = getMaxLengthWord(words);
   expect(longestWord).toBe("sentence");
 });
 
 // Тест на подсчет предложений в тексте
 test("Подсчет предложений в тексте", () => {
   const text = "This is a test sentence. Another test sentence!";
-  const sentenceCount = countSentences(text);
+  const sentenceCount = getCountSentences(text);
   expect(sentenceCount).toBe(2);
 });
 
 // Тест на подсчет количества уникальных слов в массиве слов
 test("Подсчет уникальных слов", () => {
-  const words = ["This", "is", "a", "test", "sentence", "test"];
-  const uniqueWordCount = countUniqueWords(words);
+  const words = ["This", "is", "a", "test", "sentence", "test", "Sentence"];
+  const uniqueWordCount = getCountUniqueWord(words);
   expect(uniqueWordCount).toBe(5);
 });
 
 // Тест на вывод слов массива в алфавитном порядке
 test("Вывод слов в алфавитном порядке", () => {
   const words = ["This", "is", "a", "test", "sentence"];
-  const sortedWords = displaySortedWords(words);
+  const sortedWords = getAlphabeticalOrder(words);
   expect(sortedWords).toBe("This, a, is, sentence, test");
 });
 
 // Тест на очистку таблицы с результатами
 test("Очистка таблицы с результатами", () => {
-  const answer1 = document.getElementById("answer1");
-  const answer2 = document.getElementById("answer2");
-  const answer3 = document.getElementById("answer3");
-  const answer4 = document.getElementById("answer4");
-  const answer5 = document.getElementById("answer5");
-  const answer6 = document.getElementById("answer6");
-  const answer7 = document.getElementById("answer7");
-  const answer8 = document.getElementById("answer8");
+  const countWords = document.getElementById("answer1");
+  const numOfChar = document.getElementById("answer2");
+  const longestWord = document.getElementById("answer3");
+  const reverseWord = document.getElementById("answer4");
+  const firstCharacter = document.getElementById("answer5");
+  const countSentences = document.getElementById("answer6");
+  const countUniqueWord = document.getElementById("answer7");
+  const alphabeticalOrder = document.getElementById("answer8");
 
   // Заполним некоторые значения, чтобы убедиться, что они будут очищены
-  answer1.textContent = "10";
-  answer2.textContent = "49";
-  answer3.textContent = "sentence";
-  answer4.textContent = "!ecnetnes tset rehtona tsuJ";
-  answer5.textContent = "T";
-  answer6.textContent = "2";
-  answer7.textContent = "10";
-  answer8.textContent = "Another, This, a, is, sentence, test, tsuJ";
+  countWords.textContent = "10";
+  numOfChar.textContent = "49";
+  longestWord.textContent = "sentence";
+  reverseWord.textContent = "!ecnetnes tset rehtona tsuJ";
+  firstCharacter.textContent = "T";
+  countSentences.textContent = "2";
+  countUniqueWord.textContent = "10";
+  alphabeticalOrder.textContent = "Another, This, a, is, sentence, test, tsuJ";
 
   // Вызываем функцию очистки таблицы
   clearTable();
 
   // Проверяем, что значения таблицы были очищены
-  expect(answer1.textContent).toBe("");
-  expect(answer2.textContent).toBe("");
-  expect(answer3.textContent).toBe("");
-  expect(answer4.textContent).toBe("");
-  expect(answer5.textContent).toBe("");
-  expect(answer6.textContent).toBe("");
-  expect(answer7.textContent).toBe("");
-  expect(answer8.textContent).toBe("");
+  expect(countWords.textContent).toBe("");
+  expect(numOfChar.textContent).toBe("");
+  expect(longestWord.textContent).toBe("");
+  expect(reverseWord.textContent).toBe("");
+  expect(firstCharacter.textContent).toBe("");
+  expect(countSentences.textContent).toBe("");
+  expect(countUniqueWord.textContent).toBe("");
+  expect(alphabeticalOrder.textContent).toBe("");
 });
 
 // Тест на очистку текстового поля
 test("Очистка текстового поля", () => {
-  const analyzeTitle = document.getElementById("analyze-title");
+  const userText = document.getElementById("analyze-title");
 
   // Заполним поле текстом, чтобы убедиться, что оно будет очищено
   analyzeTitle.value = "This is a test sentence. Another test sentence!";
@@ -207,5 +219,5 @@ test("Очистка текстового поля", () => {
   clearText();
 
   // Проверяем, что поле было очищено
-  expect(analyzeTitle.value).toBe("");
+  expect(userText.value).toBe("");
 });
